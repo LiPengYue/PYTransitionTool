dd6d295bb425a6f454f89023a367a10e23851e35
liPengYue <lipengyue@5lux.com>
2017-03-20 15:48:32 +0800

    分支提交

--- PYTransitionAnimation/PYTransitionAnimation/PushViewController.m
+++ PYTransitionAnimation/PYTransitionAnimation/PushViewController.m
@@ -14,33 +14,40 @@
 
 @implementation PushViewController
 
-
+//可以在懒加载的时候进行动画的设置
 - (Animatr *)animatr {
     if (!_animatr) {
         _animatr = [[Animatr alloc]init];
+        //dismiss动画预估时长
         _animatr.dismissDuration = 4;
+        
+        //present动画预估时长
         _animatr.presentDuration = 5;
-       
+        
+        //dismiss转场动画
         [_animatr dismissAnimaWithBlock:^(UIViewController *toVC, UIViewController *fromeVC, UIView *toView, UIView *fromeView) {
             NSLog(@"dismiss开始");
             [UIView animateWithDuration:_animatr.dismissDuration animations:^{
                 fromeView.frame = CGRectMake(0, 0, 100, 100);
             } completion:^(BOOL finished) {
+                //在完成动画的时候一定要把这个属性设置成YES 告诉系统动画完成
                 _animatr.isAccomplishAnima = YES;
             }];
         }];
         
+        //present转场动画
         [_animatr presentAnimaWithBlock:^(UIViewController *toVC, UIViewController *fromeVC, UIView *toView, UIView *fromeView) {
-           [UIView animateWithDuration:_animatr.presentDuration animations:^{
-               toView.frame = CGRectMake(0,300, 300, 300);
-           } completion:^(BOOL finished) {
-               _animatr.isAccomplishAnima = YES;
-           }];
-            
+            [UIView animateWithDuration:_animatr.presentDuration animations:^{
+                toView.frame = CGRectMake(0,300, 300, 300);
+            } completion:^(BOOL finished) {
+                //在完成动画的时候一定要把这个属性设置成YES 告诉系统动画完成
+                _animatr.isAccomplishAnima = YES;
+            }];
         }];
         
+        //容器视图，装有toView和fromeView，可以作为遮罩
         [_animatr setupContainerViewWithBlock:^(UIView *containerView) {
-            containerView.backgroundColor = [UIColor colorWithWhite:.1 alpha:1];
+            containerView.backgroundColor = [UIColor colorWithWhite:0.8 alpha:0.8];
         }];
         
     }
